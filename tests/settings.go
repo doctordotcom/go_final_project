7c95bec258046a586a0af719795263b02650cb3e
Daniil Kalduzov <dankalduz@gmail.com>
2025-04-27 21:37:51 +0300

    done_full

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDU1MjY2NDksImhhc2giOiIzMTMyMzMzNDM1In0.zPMlT3kJG-snC5acuukXXLrCDNGvfCSzMl1y5PYVVCQ`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDU4MDc3ODgsImhhc2giOiIzMTMyMzMzNDM1In0.bA9q6d83PyeKItHb2SubsiB6Jtc5Y6Gbx925CJbiqxU`
